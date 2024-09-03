a69b6c332d8fe9723690e9bec44729acd526b427
chetanjadhav-dev <chetanjadhav.dev@gmail.com>
2024-09-03 09:17:22 +0530

    updated models added username folder

--- instagram/models.py
+++ instagram/models.py
@@ -3,6 +3,7 @@ from django.core.files import File
 from io import BytesIO
 from PIL import Image
 import requests
+import os
 
 class InstagramProfile(models.Model):
     username = models.CharField(max_length=100, primary_key=True)
@@ -11,9 +12,13 @@ class InstagramProfile(models.Model):
     def __str__(self):
         return self.username
 
+def upload_to_user_directory(instance, filename):
+    # The image will be uploaded to MEDIA_ROOT/instagram_images/<username>/<filename>
+    return os.path.join('instagram_images', instance.profile.username, filename)
+
 class InstagramPost(models.Model):
     profile = models.ForeignKey(InstagramProfile, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(upload_to='instagram_images/')
+    image = models.ImageField(upload_to=upload_to_user_directory)
     caption = models.TextField(blank=True, null=True)
 
     def __str__(self):
