ee1b5a85a609d7b490f39e8eb9906cb811f18e05
chetanjadhav-dev <chetanjadhav.dev@gmail.com>
2024-09-04 10:46:03 +0530

    new update

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('save_posts/<str:username>/', views.save_instagram_posts, name='save_instagram_posts'),
     path('fetch_posts/<str:username>/<int:post_count>/', views.fetch_instagram_posts, name='fetch_instagram_posts'),
 ]
