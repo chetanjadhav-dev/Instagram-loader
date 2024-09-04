36cfa0fa18d81c9822323b1f5385c640efd727cc
chetanjadhav-dev <chetanjadhav.dev@gmail.com>
2024-09-04 12:32:26 +0530

    postgres connection

--- instagram/models.py
+++ instagram/models.py
@@ -6,8 +6,15 @@ import requests
 import os
 
 def user_directory_path(instance, filename):
-    # File will be uploaded to MEDIA_ROOT/instagram_images/<username>/<filename>
-    return f'instagram_images/{instance.profile.username}/{filename}'
+    # Determine the folder based on the media type
+    media_type_folder = 'images'  # Default folder is for images
+    if instance.media_type == 'video':
+        media_type_folder = 'videos'
+    elif instance.media_type == 'carousel':
+        media_type_folder = 'carousels'
+    
+    # File will be uploaded to MEDIA_ROOT/instagram_images/<username>/<media_type>/<filename>
+    return f'instagram_images/{instance.profile.username}/{media_type_folder}/{filename}'
 
 class InstagramProfile(models.Model):
     username = models.CharField(max_length=100, primary_key=True)
@@ -17,8 +24,16 @@ class InstagramProfile(models.Model):
         return self.username
 
 class InstagramPost(models.Model):
+    MEDIA_TYPE_CHOICES = [
+        ('image', 'Image'),
+        ('video', 'Video'),
+        ('carousel', 'Carousel'),
+    ]
+
     profile = models.ForeignKey(InstagramProfile, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(upload_to=user_directory_path)
+    media_type = models.CharField(max_length=10, choices=MEDIA_TYPE_CHOICES, default='image')
+    image = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+    video = models.FileField(upload_to=user_directory_path, blank=True, null=True)
     caption = models.TextField(blank=True, null=True)
 
     def __str__(self):
--- instagram/views.py
+++ instagram/views.py
@@ -35,16 +35,55 @@ def save_instagram_posts(request, username):
         if i >= post_limit:
             break
         
-        img = download_image(post.url)
-        img_io = BytesIO()
-        img.save(img_io, format=img.format)
-        img_file = ContentFile(img_io.getvalue(), f"{username}_{i}.{img.format.lower()}")
+        if post.typename == 'GraphImage':
+            # Handle single image post
+            img = download_image(post.url)
+            img_io = BytesIO()
+            img.save(img_io, format=img.format)
+            img_file = ContentFile(img_io.getvalue(), f"{username}_{i}.{img.format.lower()}")
 
-        InstagramPost.objects.create(
-            profile=profile_obj,
-            image=img_file,
-            caption=post.caption
-        )
+            InstagramPost.objects.create(
+                profile=profile_obj,
+                media_type='image',
+                image=img_file,
+                caption=post.caption
+            )
+
+        elif post.typename == 'GraphVideo':
+            # Handle video post
+            video_response = requests.get(post.video_url)
+            if video_response.status_code == 200:
+                video_file = ContentFile(video_response.content, f"{username}_{i}.mp4")
+
+                InstagramPost.objects.create(
+                    profile=profile_obj,
+                    media_type='video',
+                    video=video_file,
+                    caption=post.caption
+                )
+        
+        elif post.typename == 'GraphSidecar':
+            # Handle carousel post
+            for j, sidecar in enumerate(post.get_sidecar_nodes()):
+                if sidecar.is_video:
+                    media_response = requests.get(sidecar.video_url)
+                    media_type = 'video'
+                    ext = 'mp4'
+                else:
+                    media_response = requests.get(sidecar.display_url)
+                    media_type = 'image'
+                    ext = 'jpg'
+                
+                if media_response.status_code == 200:
+                    media_file = ContentFile(media_response.content, f"{username}_{i}_carousel_{j}.{ext}")
+
+                    InstagramPost.objects.create(
+                        profile=profile_obj,
+                        media_type=media_type,
+                        image=media_file if media_type == 'image' else None,
+                        video=media_file if media_type == 'video' else None,
+                        caption=post.caption
+                    )
 
     return render(request, 'instagram/success.html', {'profile': profile_obj})
 
